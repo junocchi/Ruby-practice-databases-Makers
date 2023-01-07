33400d9f568023dc07fb6de54ad8d328d90b6cf1
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-01-07 18:09:41 +0000

    Updated comments

--- Music_library/lib/album_repository.rb
+++ Music_library/lib/album_repository.rb
@@ -22,15 +22,3 @@ class AlbumRepository
     return albums
   end
 end
-
-# Quando chamei essa função
-# repo = AlbumRepository.new
-# albums = repo.all
-# deu erro no terminal dizendo que 
-# album_repository.rb:10:in `all': uninitialized constant 
-# AlbumRepository::DatabaseConnection (NameError)
-#	from album_repository.rb:27:in `<main>'
-
-# Note that the all method depends on the DatabaseConnection class. 
-# You will need to make sure that the DatabaseConnection class 
-# is defined and available in your code before calling the all method.
